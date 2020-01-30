3f4e84216adfd3e726c0c1b870789fc597735797
jdluong <luongjohnd@gmail.com>
2020-01-29 21:37:09 -0800

    organized into modules; but classes, or modules?

--- api.py
+++ api.py
@@ -6,7 +6,8 @@ class API:
     PASSWORD = Keys.PASSWORD
     SHARED_SECRET = Keys.SHARED_SECRET
     API_VERSION = "2020-01"
+    STORE_NAME = "ethic-blueprint"
 
-    ADMIN_URL = "https://%s:%s@ethictest.myshopify.com/admin/api/%s" % (API_KEY, PASSWORD, API_VERSION)
-    BLOG_URL = ADMIN_URL+"/blogs.json"
-    ARTICLE_URL_post = lambda ADMIN_URL, blog_id: f"{ADMIN_URL}/blogs/{blog_id}/articles.json"
+    ADMIN_URL = f"https://{API_KEY}:{PASSWORD}@{STORE_NAME}.myshopify.com/admin/api/{API_VERSION}"
+    # BLOG_URL = ADMIN_URL+"/blogs.json"
+    # ARTICLE_URL_post = lambda ADMIN_URL, blog_id: f"{ADMIN_URL}/blogs/{blog_id}/articles.json"
--- api_test.py
+++ api_test.py
@@ -4,20 +4,30 @@ from api import API
 from typing import List
 import mysql.connector as sql
 
-def get_blogs() -> List[tuple]:
+def get_blogs() -> dict:
     """ Gets ID and Title of all blogs
     Args:
         N/A
     Returns:
-        Tuple of blog info (id,title)
+        (dict) of blog info {title:id}
     """
     r = requests.get(API.BLOG_URL)
-    blogs = []
+    print(r.status_code)
+    blogs = {}
     # print(r_get_blogs.status_code,r_get_blogs.json())
     for blog in r.json()['blogs']:
-        blogs.append((blog['id'],blog['title']))
+        blogs['title'] = blog['id']
     
     return blogs
+
+def get_blog_id_by_title(title: str) -> int:
+    """ Gets blog ID given a title
+    Args:
+        title (str): title of blog to get
+    Returns:
+        (int) the ID of the blog
+    """
+    return get_blogs().
 
 def get_articles() -> List[List]:
     """
@@ -32,16 +42,17 @@ def post_articles(blog_id: str, data: dict):
 
 
 if __name__ == '__main__':
-    data = {
-            "title": "API test",
-            "author": "John Luong",
-            "tags": "api, tag1, tag2",
-            "body_html": "<h1>api test post</h1>\n<p><strong>YEET</strong></p>",
-            "published_at": "Thu Mar 24 15:45:47 UTC 2011"
-            }
+    # data = {
+    #         "title": "API test",
+    #         "author": "John Luong",
+    #         "tags": "api, tag1, tag2",
+    #         "body_html": "<h1>api test post</h1>\n<p><strong>YEET</strong></p>",
+    #         "published_at": "Thu Mar 24 15:45:47 UTC 2011"
+    #         }
+    # blog_id = ""
             
-    print(post_articles("53068562571",data).status_code)
-
+    # print(post_articles(blog_id,data).status_code)
+    print(get_blogs())
 
 
 
